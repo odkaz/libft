fae9442839118713c9412af8abfff0afff99c839
odkaz <one.destiny.japan@gmail.com>
2020-07-07 15:17:27 +0900

    only split

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,49 @@
 /*   By: kazumanoda <kazumanoda@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 23:27:58 by knoda             #+#    #+#             */
-/*   Updated: 2020/07/05 19:41:41 by kazumanoda       ###   ########.fr       */
+/*   Updated: 2020/07/05 21:33:31 by kazumanoda       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static int	cnt_mal(char const *s, char c)
+{
+	int	cnt;
+	int	flag;
+
+	cnt = 1;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		flag = 0;
+		while (*s != c && *s)
+		{
+			flag = 1;
+			s++;
+		}
+		if (flag)
+			cnt++;
+	}
+	return (cnt);
+}
+
+static void	*free_all(char **res, int index)
+{
+	while (index--)
+		free(res[index]);
+	free(res);
+	return (NULL);
+}
+
+char		**ft_split(char const *s, char c)
 {
 	char	**res;
 	int		index;
 	int		i;
 
-	res = (char	**)ft_calloc(((ft_strlen(s) / 2 + 1) + 1), sizeof(char	*));
+	res = (char	**)ft_calloc(cnt_mal(s, c), sizeof(char	*));
 	if (!res)
 		return (NULL);
 	index = 0;
@@ -28,8 +58,12 @@ char	**ft_split(char const *s, char c)
 		while (!ft_memchr(s, c, i + 1) && s[i])
 			i++;
 		if (i > 0)
-			res[index++] = ft_substr(s, 0, i);
-		s += i;
+		{
+			res[index] = ft_substr(s, 0, i);
+			if (res[index++] == NULL)
+				free_all(res, --index);
+			s += i;
+		}
 		while (*s == c && *s)
 			s++;
 	}
