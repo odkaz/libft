1676f3859464283a0157be2bcd81e6c7865cdfd7
Kazuma Noda <knoda@c1r31s11.42tokyo.jp>
2021-11-18 14:04:37 +0900

    exit at ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: knoda <knoda@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 23:25:06 by knoda             #+#    #+#             */
-/*   Updated: 2021/11/18 13:49:37 by knoda            ###   ########.fr       */
+/*   Updated: 2021/11/18 14:04:00 by knoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ void	*ft_calloc(size_t n, size_t size)
 	res = (void *)malloc(size * n);
 	if (res)
 		ft_bzero(res, size * n);
+	else
+		exit(1);
 	return (res);
 }
